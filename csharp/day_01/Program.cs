04489bde802d224a73cf86c8af578e0325e2e3a1
Matheus Leal da Silva <theusleals@outlook.com>
2024-12-02 22:19:40 +0000

    Day 01, part 01

---
+++ csharp/day_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+
+using Day_01.Services;
+
+Collection<int> leftRaw = [];
+Collection<int> rightRaw = [];
+
+IOService
+.ReadFile("./data/location_id_list.txt", (reader) => {
+    var line = reader.ReadLine();
+
+    if (line != null)
+    {
+        var split = line.Split("   ");
+        leftRaw.Add(int.Parse(split[0]));
+        rightRaw.Add(int.Parse(split[1]));
+    }
+});
+
+var leftList = leftRaw.OrderBy(x => x).ToList();
+var rightList = rightRaw.OrderBy(x => x).ToList();
+var total = 0;
+
+for (int i = 0; i < leftList.Count; i++)
+{
+   total += Math.Abs(leftList[i] - rightList[i]); 
+}
+
+Console.WriteLine("Total: {0}", total);
